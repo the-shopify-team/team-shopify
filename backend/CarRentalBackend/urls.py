60eb04a2ed6a3b1f8c18c6a65b449daba3d949b8
battleangel217 <idaraobong05@gmail.com>
2025-10-02 12:37:13 +0100

    Versioned the api

--- backend/CarRentalBackend/urls.py
+++ backend/CarRentalBackend/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt'))
+    path('api/v1/', include([
+        path('auth/', include('djoser.urls')),
+        path('auth/', include('djoser.urls.jwt'))
+    ])
+    )
 ]
