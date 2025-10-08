5255f16a55aedda477ec73739577179d694e7551
Chiamaka <lillyannmaka@gmail.com>
2025-10-08 05:50:18 -0400

    feat: create layout for auth screens

--- frontend/car-rental-app/src/app/(auth)/layout.tsx
+++ frontend/car-rental-app/src/app/(auth)/layout.tsx
@@ -1,11 +1,24 @@
+import Image from 'next/image'
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>{children}</main>
+    <div className='flex min-h-screen w-full'>
+        <main className='flex-1 overflow-y-auto flex justify-center items-center'>
+            <div className='w-full max-w-md'>{children}</div>
+        </main>
+        <div className='flex-1 relative overflow-hidden hidden lg:block'>
+            <Image
+               src="/images/auth-image.png"
+               alt="Picture of a car"
+               fill
+               className="object-cover"
+               priority
+            />
+        </div>
     </div>
   );
 }
